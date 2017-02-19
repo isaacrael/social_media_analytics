f8622258849ee7b296cdc810437e777cee741206
isaacrael <grael@comcast.net>
2017-02-19 12:02:02 -0700

    "my first commit"

---
+++ social_media_analytics/social_media_analytics_app/tweepy_stream.py
@@ -0,0 +1,26 @@
+"""
+
+Written By: Gil Rael
+
+
+The following application provides social media analytics from Twitter via tweepy
+
+
+"""
+
+
+
+import tweepy
+
+
+consumer_key = "JRSzaMcADzpjqWiu8mviGeJWx"
+
+consumer_secret = "T0gDLHl7o9FYx0VJ18h6oiR68kPwi7qIcLoeHGQEWBMmj7k2vj"
+
+access_token = "100592963-hFkQOpwfnhksoV8kywWOQqTDeKrE863qrtgmCNKn"
+
+access_token_secret = "T0gDLHl7o9FYx0VJ18h6oiR68kPwi7qIcLoeHGQEWBMmj7k2vj"
+
+
+
+
