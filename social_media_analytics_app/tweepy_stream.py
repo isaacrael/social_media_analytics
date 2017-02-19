73aad01667217a831e089bc04b586d5453f652b4
isaacrael <grael@comcast.net>
2017-02-18 20:34:07 -0700

    "added variables"

--- social_media_analytics_app/tweepy_stream.py
+++ social_media_analytics_app/tweepy_stream.py
@@ -1,1 +1,26 @@
-__author__ = 'isaac'
+"""
+
+Written By: Gil Rael
+
+
+The following application provides social media analytics from Tweeter via tweepy
+
+
+"""
+
+
+
+import tweepy
+
+
+consumer_key = "JRSzaMcADzpjqWiu8mviGeJWx"
+
+consumer_secret = "T0gDLHl7o9FYx0VJ18h6oiR68kPwi7qIcLoeHGQEWBMmj7k2vj"
+
+access_token = "100592963-hFkQOpwfnhksoV8kywWOQqTDeKrE863qrtgmCNKn"
+
+access_token_secret = "T0gDLHl7o9FYx0VJ18h6oiR68kPwi7qIcLoeHGQEWBMmj7k2vj"
+
+
+
+
