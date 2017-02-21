8e8023d429a7385dad563f5eb5aa9550a9752bb6
isaacrael <grael@comcast.net>
2017-02-20 18:04:56 -0700

    "removed the registration app from settings.py - removed URL that did not work from urls.py"

--- social_media_analytics/social_media_analytics/urls.py
+++ social_media_analytics/social_media_analytics/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'social_media_analytics_app.index', name='index'),
+    url(r'^$', include('social_media_analytics_app.urls', namespace="social_media_analytics_app")),
 
 ]
