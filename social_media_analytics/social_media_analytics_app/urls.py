28b8f7a9fc7c183ed33a8d3d4612f92fe709241f
isaacrael <grael@comcast.net>
2017-02-22 17:39:15 -0700

    "added include for send emails url"

--- social_media_analytics/social_media_analytics_app/urls.py
+++ social_media_analytics/social_media_analytics_app/urls.py
@@ -1,13 +1,18 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
 
 urlpatterns = [
     # ex: /quiz/
+    url(r'^', include('send_email.urls')),
     url(r'^$', views.index, name='index'),
     url(r'^analytics/', views.analytics, name='analytics'),
-    url(r'^contact/', views.contact, name='contact'),
+    url(r'^twitter/', views.twitter_sentiment, name='twitter_sentiment'),
+    url(r'^facebook/', views.facebook_sentiment, name='facebook_sentiment'),
+    url(r'^youtube/', views.youtube_sentiment, name='youtube_sentiment'),
+    url(r'^tumblr/', views.tumblr_sentiment, name='tumblr_sentiment'),
+    url(r'^contact/', views.contact, name='contact')
     #url(r'^resources/', views.git_resources, name='resources'),
     # ex: /quiz/5/
     #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
