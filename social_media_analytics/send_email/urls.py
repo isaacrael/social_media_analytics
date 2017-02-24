ecae5f206534348536dc6c91aaa2ca90c88d57d7
isaacrael <grael@comcast.net>
2017-02-23 17:13:12 -0700

    "added the contact url"

--- social_media_analytics/send_email/urls.py
+++ social_media_analytics/send_email/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include, url
 
 from . import views
 
+
 urlpatterns = [
 #    url(r'^', include('send_email.urls')),
-    url(r'^email/', views.email, name='email'),
-    url(r'^success/', views.success, name='success'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^success/', views.success, name='success')
 ]
