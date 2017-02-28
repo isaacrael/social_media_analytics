3985f70a4ad65bae22ab5795212b6830cd047cbf
isaacrael <grael@comcast.net>
2017-02-27 18:38:11 -0700

    "fixed email function"

--- social_media_analytics/send_email/views.py
+++ social_media_analytics/send_email/views.py
@@ -8,20 +8,21 @@ from .forms import ContactForm
 # create your own views here
 
 def email(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
+    if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['gilrael79@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('success')
-    return render(request, "email.html", {'form': form})
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['gil@gilrael.com'],
+            )
+            return HttpResponseRedirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'email.html', {'form': form})
+
 
 
 def success(request):
